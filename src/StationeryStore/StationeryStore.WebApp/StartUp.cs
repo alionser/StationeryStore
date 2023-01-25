1f97102b43540f1cf16759d5c6765bddbbaf4864
alionser <smirnoviksa@gmail.com>
2023-01-25 06:40:50 +0500

    WIP

--- src/StationeryStore/StationeryStore.WebApp/StartUp.cs
+++ src/StationeryStore/StationeryStore.WebApp/StartUp.cs
@@ -1,4 +1,8 @@
-﻿namespace StationeryStore.WebApp;
+﻿using Autofac;
+using Microsoft.EntityFrameworkCore;
+using StationeryStore.Data;
+
+namespace StationeryStore.WebApp;
 
 public sealed class StartUp
 {
@@ -8,12 +12,35 @@ public sealed class StartUp
     {
         Configuration = configuration;
     }
-    
+
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddRazorPages();
+
+        var connectionString = Configuration.GetConnectionString("DefaultConnection");
+        
+        
+        services.AddDbContext<DataContext>(
+            options =>
+            {
+                options.UseNpgsql(connectionString, b =>
+                {
+                    b.MigrationsAssembly("StationeryStore.Data");
+                });
+            },
+            ServiceLifetime.Transient);
+
+        // services.AddDbContext<DataContext>(options => { options.UseNpgsql(connectionString); },
+        // ServiceLifetime.Transient);
     }
-    
+
+    public void ConfigureContainer(ContainerBuilder builder)
+    {
+        builder.RegisterType(typeof(DataContext))
+            .As<DataContext>()
+            .InstancePerLifetimeScope();
+    }
+
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         if (env.IsDevelopment())
@@ -28,11 +55,11 @@ public sealed class StartUp
 
         app.UseHttpsRedirection();
         app.UseStaticFiles();
-        
+
         app.UseRouting();
-        app.UseEndpoints(builder =>
-        {
-            builder.MapRazorPages();
-        });
+        app.UseEndpoints(builder => { builder.MapRazorPages(); });
+        
+        var dataContext = app.ApplicationServices.GetRequiredService<DataContext>();
+        dataContext.Database.Migrate();
     }
 }
